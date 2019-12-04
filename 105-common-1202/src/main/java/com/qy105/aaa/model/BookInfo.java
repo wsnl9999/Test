0c66dcb88c12a227239c8f9ce4961340a12f734c
小男神 <2296868962@qq.com>
2019-12-04 21:00:16 +0800

    我这是第一次提交

---
+++ 105-common-1202/src/main/java/com/qy105/aaa/model/BookInfo.java
@@ -0,0 +1,15 @@
+package com.qy105.aaa.model;
+
+import lombok.Data;
+
+/**
+ * @author 小男神
+ */
+@Data
+public class BookInfo {
+    private int bookId;
+    private String bookName;
+    private int bookStore;
+    private int bookPrice;
+
+}
